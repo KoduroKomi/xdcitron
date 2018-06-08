d15ee413ae5cafe2d6e5e66a039272677de7da29
KoduroKomi <36133652+KoduroKomi@users.noreply.github.com>
2018-06-08 20:54:10 +0200

    Create bot.js

---
+++ .gitignore/bot.js
@@ -0,0 +1,24 @@
+const Discord = require ('discord.js');
+const bot = new Discord.Client();
+const botsettings = require("./botsettings.json");
+
+bot.on('ready', function() {
+    bot.user.setGame("Dire bienvenue");
+    console.log("XD-CITRON Connecté !")
+});
+
+bot.login(botsettings.token);
+
+bot.on("guildMemberAdd", member => {
+    let role = member.guild.roles.find("name","👁‍🗨*~Spéctateurs~*👁‍🗨")
+    member.guild.channels.find("name","annonces")
+    .sendMessage(":lemon: " + member.toString() + " a rejoins le serveur !")
+    member.addRole(role)
+    console.log("Nouveau membre !")
+})
+
+bot.on("guildMemberRemove", member => {
+    member.guild.channels.find("name","annonces")
+    .send(`:lemon: ** ${member.user.username} ** a quitté le serveur.`)
+    console.log("Quelqu'un a leave.")
+})
